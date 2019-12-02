adf2357f1eaab446a54e6d72bcc2caa2a5203831
erick mendieta <erick84mm@gmail.com>
2019-12-02 11:45:23 +0900

    debugging...

--- habitat/datasets/vln/r2r_dataset.py
+++ habitat/datasets/vln/r2r_dataset.py
@@ -80,7 +80,9 @@ class R2RDatasetV1(Dataset):
                         len(DEFAULT_SCENE_PATH_PREFIX) :
                     ]
                 episode.scene_id = os.path.join(scenes_dir, episode.scene_id)
-            episode.instruction = InstructionData(**episode.instruction)
+            episode.instruction = InstructionData(
+                instruction=episode.instruction
+            )
 
             for v_index, viewpoint in enumerate(episode.path):
                 scan = episode.scan
