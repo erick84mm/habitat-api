5e48a7c9418c7fa669085cdae35b0320f3393dfd
erick mendieta <erick84mm@gmail.com>
2019-12-05 16:55:33 +0900

    adding the adjacentViewpoint sensor

--- habitat/tasks/vln/vln.py
+++ habitat/tasks/vln/vln.py
@@ -217,8 +217,7 @@ class AdjacentViewpointSensor(Sensor):
         opposite_angle = 2 * np.pi - angle
         target_angle = self._config.HFOV * 2 * np.pi / 360 / 2
 
-        if angle <= target_angle or
-        opposite_angle <= target_angle:
+        if angle <= target_angle or opposite_angle <= target_angle:
                 return True
         return False
 
