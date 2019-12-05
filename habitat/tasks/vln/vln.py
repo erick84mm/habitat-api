a6ca1d248df336113aaf4b878d31ff146de36d4d
erick mendieta <erick84mm@gmail.com>
2019-12-05 17:18:16 +0900

    debugging

--- habitat/tasks/vln/vln.py
+++ habitat/tasks/vln/vln.py
@@ -250,6 +250,7 @@ class AdjacentViewpointSensor(Sensor):
     def get_observation(
         self, observations, episode, *args: Any, **kwargs: Any
     ):
+        kwargs["scan"] = episode.scan
         abjacent_viewpoints = _get_observation_space(scan=episode.scan)
         accessible_viewpoints = []
         for viewpoint in abjacent_viewpoints:
