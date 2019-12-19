c74e36be5a783f50bc2982ebea42fd5cb67af6bd
erick mendieta <erick84mm@gmail.com>
2019-12-19 17:47:02 +0900

    The solution is that the Matterport simulator relative heading doesn't look at the center but at the range between the center and half the fov angle

--- examples/vln_benchmark.py
+++ examples/vln_benchmark.py
@@ -495,7 +495,7 @@ def main():
     #metrics = benchmark.evaluate(agent, num_episodes=args.num_episodes)
 
 
-    agent = ShortestPathAgent(3.0, "SPL", half_visible_angle=0.698132)
+    agent = ShortestPathAgent(3.0, "SPL", half_visible_angle=0.785398)
     benchmark = VLNShortestPathBenchmark(args.task_config)
     metrics = benchmark.evaluate(agent, num_episodes=args.num_episodes)
 
