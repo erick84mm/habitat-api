fixing _is_navigable function to match rel_heading calculation

habitat/tasks/vln/vln.py
@@ -227,7 +227,7 @@ class AdjacentViewpointSensor(Sensor):
 
     def get_relative_heading(self, posA, rotA, posB, half_visible_angle):
         direction_vector = np.array([0, 0, -1])
-        quat = quaternion_from_coeff(rotA).inverse()
+        quat = rotA.inverse()
         heading_vector = quaternion_rotate_vector(quat, direction_vector)
         heading_angle = cartesian_to_polar(-heading_vector[2], heading_vector[0])[1]
         target_vector = np.array(posB) - np.array(posA)