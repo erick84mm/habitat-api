5ad6a935645291c43c1772060fef87384e1cd7df
erick mendieta <erick84mm@gmail.com>
2019-12-22 17:25:36 +0900

    removing pi/12 to make it the same as matterport

--- habitat/tasks/vln/vln.py
+++ habitat/tasks/vln/vln.py
@@ -331,7 +331,7 @@ class AdjacentViewpointSensor(Sensor):
         #print("elevation_angle_2", elevation_angle_2)
         #print("rel_elevation", rel_elevation)
         print("viewpoint %s" % c, rel_elevation - elevation_angle)
-        return rel_elevation - elevation_angle - np.pi / 12
+        return rel_elevation - elevation_angle 
 
     def _get_observation_space(self, *args: Any, **kwargs: Any):
         observations = []
