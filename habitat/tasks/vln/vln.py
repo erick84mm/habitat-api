149d75d52310ffc7abb7137d9f1be9a14ad98e65
erick mendieta <erick84mm@gmail.com>
2019-12-26 18:54:17 +0900

    minor fix in the measure

--- habitat/tasks/vln/vln.py
+++ habitat/tasks/vln/vln.py
@@ -622,7 +622,7 @@ class NavigationError(Measure):
         current_position = self._sim.get_agent_state().position.tolist()
 
         distance_to_target = self._sim.geodesic_distance(
-            current_position, episode.goals[-1].view_point.position
+            current_position, episode.goals[-1].view_point.get_position()
         )
         self._previous_position = current_position
 
@@ -674,7 +674,7 @@ class DistanceToGoal(Measure):
     def reset_metric(self, episode, *args: Any, **kwargs: Any):
         self._previous_position = self._sim.get_agent_state().position.tolist()
         self._start_end_episode_distance = self._sim.geodesic_distance(
-            self._previous_position, episode.goals[0].view_point.position
+            self._previous_position, episode.goals[-1].view_point.get_position()
         )
         self._agent_episode_distance = 0.0
         self._metric = None
@@ -688,7 +688,7 @@ class DistanceToGoal(Measure):
         current_position = self._sim.get_agent_state().position.tolist()
 
         distance_to_target = self._sim.geodesic_distance(
-            current_position, episode.goals[0].view_point.position
+            current_position, episode.goals[-1].view_point.get_position()
         )
 
         self._agent_episode_distance += self._euclidean_distance(
