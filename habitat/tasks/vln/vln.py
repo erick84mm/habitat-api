cba13f24b5b3bd55d2c59d006b8df9e0d7d5a54b
erick mendieta <erick84mm@gmail.com>
2020-01-02 21:01:23 +0900

    adding snap_point mechanic to teleport in case of an error

--- habitat/tasks/vln/vln.py
+++ habitat/tasks/vln/vln.py
@@ -724,7 +724,7 @@ class TeleportAction(SimulatorTaskAction):
 
         if not self._sim.is_navigable(position):
             # is not navigable then we search for a location close to the target
-            new_position = np.ndarray(shape=(3,1), dtype=float32, buffer=np.array(position))
+            new_position = np.ndarray(shape=(3,1), dtype='f', buffer=np.array(position))
             new_position = PathFinder.snap_point(new_position)
             if np.isnan(new_position[0]):
                 return self._sim.get_observations_at()
