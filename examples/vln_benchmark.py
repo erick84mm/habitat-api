1966c7b7085cc5d0a3d8fae8f20f2b672861cbce
erick mendieta <erick84mm@gmail.com>
2020-01-05 11:23:21 +0900

    fixing final print

--- examples/vln_benchmark.py
+++ examples/vln_benchmark.py
@@ -139,7 +139,7 @@ class RandomBenchmark(VLNBenchmark):
                 count_episodes += 1
                 print(count_episodes)
 
-            avg_metrics = {k: v / count_episodes for k, v in agg_metrics.items()}
+            avg_metrics = {k: v / count_episodes for k, v in self.agg_metrics.items()}
 
             return avg_metrics
 
@@ -235,7 +235,7 @@ class DiscreteRandomBenchmark(VLNBenchmark):
                 count_episodes += 1
                 print(count_episodes)
 
-            avg_metrics = {k: v / count_episodes for k, v in agg_metrics.items()}
+            avg_metrics = {k: v / count_episodes for k, v in self.agg_metrics.items()}
 
             return avg_metrics
 
@@ -524,11 +524,95 @@ class DiscreteShortestPathAgent(habitat.Agent):
 
 class seq2seqAgent(habitat.Agent):
     def __init__(self, success_distance, goal_sensor_uuid, encoder, decoder):
+
+        self.model_actions = ['TURN_LEFT', 'TURN_RIGHT', 'LOOK_UP', 'LOOK_DOWN', 'MOVE_FORWARD', 'STOP', '<start>', '<ignore>']
         self.dist_threshold_to_stop = success_distance
         self.goal_sensor_uuid = goal_sensor_uuid
         self.encoder = encoder
         self.decoder = decoder
         self.criterion = nn.CrossEntropyLoss()
+        self.losses = []
+
+    def reset(self):
+        pass
+
+    def is_goal_reached(self, observations):
+        dist = observations[self.goal_sensor_uuid][0]
+        return dist <= self.dist_threshold_to_stop
+
+    def _teacher_actions(self):
+        return []
+'''
+    def act(self, observations, action, episode):
+
+        # Initialization when the action is start
+        batch_size = 1
+        seq = episode.instruction.tokens
+        seq_mask = episode.instruction.mask
+        seq_length = episode.instruction.tokens_length
+
+        # Forward through encoder, giving initial hidden state and memory cell for decoder
+        ctx,h_t,c_t = self.encoder(seq, seq_lengths)
+
+        # Initial action
+        a_t = Variable(torch.ones(batch_size).long() * self.model_actions.index('<start>'),
+                    requires_grad=False).cuda()
+        ended = np.array([False] * batch_size) # Indices match permuation of the model, not env
+
+        # Training cycle until stop action is predicted.
+
+        # Do a sequence rollout and calculate the loss
+        self.loss = 0
+        f_t = self._feature_variable(perm_obs) # Image features from obs
+        h_t,c_t,alpha,logit = self.decoder(a_t.view(-1, 1), f_t, h_t, c_t, ctx, seq_mask)
+            # Mask outputs where agent can't move forward
+        for i, ob in enumerate(perm_obs):
+            if len(ob['navigableLocations']) <= 1:
+                logit[i, self.model_actions.index('MOVE_FORWARD')] = -float('inf')
+
+            # Supervised training
+            target = self._teacher_action(perm_obs, ended)
+            self.loss += self.criterion(logit, target)
+
+            # Determine next model inputs
+            if self.feedback == 'teacher':
+                a_t = target                # teacher forcing
+            elif self.feedback == 'argmax':
+                _,a_t = logit.max(1)        # student forcing - argmax
+                a_t = a_t.detach()
+            elif self.feedback == 'sample':
+                probs = F.softmax(logit, dim=1)
+                m = D.Categorical(probs)
+                a_t = m.sample()            # sampling an action from model
+            else:
+                sys.exit('Invalid feedback option')
+
+            # Updated 'ended' list and make environment action
+            for i,idx in enumerate(perm_idx):
+                action_idx = a_t[i].item()
+                if action_idx == self.model_actions.index('<end>'):
+                    ended[i] = True
+                env_action[idx] = self.env_actions[action_idx]
+
+            obs = np.array(self.env.step(env_action))
+            perm_obs = obs[perm_idx]
+
+            # Save trajectory output
+            for i,ob in enumerate(perm_obs):
+                if not ended[i]:
+                    traj[i]['path'].append((ob['viewpoint'], ob['heading'], ob['elevation']))
+
+            # Early exit if all ended
+            if ended.all():
+                break
+
+        self.losses.append(self.loss.item() / self.episode_len)
+        return traj
+
+        action_args = {}
+        return {"action": action, "action_args": action_args}
+'''
+
 
 
 def main():
