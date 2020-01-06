46bafec48b5c847010bbbcf69ce22bfabfac4785
erick mendieta <erick84mm@gmail.com>
2020-01-06 13:16:53 +0900

    adding prints, pprint

--- examples/vln_seq2seq.py
+++ examples/vln_seq2seq.py
@@ -138,6 +138,7 @@ class Seq2SeqBenchmark(VLNBenchmark):
 
                 action_history.append({
                     "action": action["action"],
+                    "action_args": action["action_args"],
                     "prev_image_id": prev_image_id,
                     "prev_heading": prev_heading,
                     "prev_pos": prev_state.position,
