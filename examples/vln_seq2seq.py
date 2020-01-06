93521a64441abd3c42c4c6f44d5a5db90d08357f
erick mendieta <erick84mm@gmail.com>
2020-01-06 13:55:35 +0900

    adding a print for the metrics at the end to see the losses of each episode

--- examples/vln_seq2seq.py
+++ examples/vln_seq2seq.py
@@ -240,6 +240,8 @@ def main():
 
     metrics = benchmark.train(agent, num_episodes=args.num_episodes)
 
+    for k, v in metrics.items():
+        print("{}: {:.3f}".format(k, v))
 
 if __name__ == "__main__":
     main()
