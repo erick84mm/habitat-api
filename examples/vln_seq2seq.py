948a9671cbcee62fdd14080fc996af81290424d3
erick mendieta <erick84mm@gmail.com>
2020-01-08 09:36:23 +0900

    removing train step from evaluation

--- examples/vln_seq2seq.py
+++ examples/vln_seq2seq.py
@@ -244,7 +244,6 @@ class Seq2SeqBenchmark(VLNBenchmark):
             #pprint(self._env._current_episode)
             #pprint(self._env._current_episode.goals)
             #pprint(action_history)
-            agent.train_step(count_episodes)
             count_episodes += 1
             metrics = self._env.get_metrics()
             for m, v in metrics.items():
