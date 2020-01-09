b4b5127997fb4f2736fea0940116521090409394
erick mendieta <erick84mm@gmail.com>
2020-01-10 02:33:40 +0900

    removing Variable wrapper

--- habitat_baselines/vln/agents.py
+++ habitat_baselines/vln/agents.py
@@ -249,7 +249,7 @@ class seq2seqAgent(habitat.Agent):
                     # Keeping the same rotation as the previous step
                     # camera rotation
                     rot = observations["adjacentViewpoints"][0][14:18]
-                    print("Teleporting to ",image_id, pos, rot, ob)
+                    print("Teleporting to ",image_id, goal, pos, rot, ob)
                     viewpoint = ViewpointData(
                         image_id=image_id,
                         view_point=AgentState(position=pos, rotation=rot)
