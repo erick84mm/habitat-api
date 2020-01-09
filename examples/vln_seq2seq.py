fixing the target logit so the calculation of the loss can be done well now

examples/vln_seq2seq.py
@@ -297,7 +297,7 @@ def main():
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     print(device)
 
-    encoder = EncoderLSTM(1300, 256, 256, 0, 0.5, bidirectional=False, num_layers=2).to(device)
+    encoder = EncoderLSTM(1300, 256, 256, 0, 0.5, bidirectional=False, num_layers=1).to(device)
     decoder = AttnDecoderLSTM(8, 6, 32, 256, 0.5).to(device)
 
     agent = seq2seqAgent(3.0, "SPL", encoder, decoder)

habitat_baselines/vln/agents.py
@@ -59,7 +59,7 @@ class seq2seqAgent(habitat.Agent):
 
         # Initializing resnet152 model
         self.image_model = models.resnet152(pretrained=True)
-        self.image_model=nn.Sequential(*list(self.image_model.children())[:-1])
+        self.image_model = nn.Sequential(*list(self.image_model.children())[:-1])
         for p in self.image_model.parameters():
             p.requires_grad = False
 
@@ -175,6 +175,7 @@ class seq2seqAgent(habitat.Agent):
 
         im = observations["rgb"][:,:,[2,1,0]]
         f_t = self._get_image_features(im) #.to('cuda')
+        f_t = Variable(torch.from_numpy(f_t), requires_grad=False).cuda()
 
         ended = np.array([False] * batch_size) # Indices match permuation of the model, not env
 
@@ -196,7 +197,8 @@ class seq2seqAgent(habitat.Agent):
 
         # Supervised training
         target_action, action_args = self._teacher_actions(observations, goal)
-        target = torch.LongTensor([self.model_actions.index(target_action)])
+        target = torch.LongTensor(1)
+        target[0] = self.model_actions.index(target_action)
         target = Variable(target, requires_grad=False).to('cuda')
         self.loss += self.criterion(logit, target)
         #print(logit)

habitat_baselines/vln/common/seq2seq.py
@@ -1,13 +1,15 @@
 import torch
 import torch.nn as nn
+from habitat_baselines.vln.models import visual_encoder
 
 
-class Seq2Seq(nn.Module):
-    def __init__(self, encoder, decoder, dim_actions):
+class Seq2SeqActor(nn.Module):
+    def __init__(self, observation_space, encoder, decoder, dim_actions):
         super().__init__()
         self.encoder = encoder
         self.decoder = decoder
         self.criterion = nn.CrossEntropyLoss()
+        self.visual_encoder = visual_encoder.ResNet(observation_space)
 
     def forward(self, *x):
         raise NotImplementedError
@@ -20,6 +22,10 @@ class Seq2Seq(nn.Module):
         masks,
         deterministic=False,
     ):
+    
+        ctx, h_t, c_t = self.encoder(seq, seq_lengths)
+
+
         features, rnn_hidden_states = self.net(
             observations, rnn_hidden_states, prev_actions, masks
         )

habitat_baselines/vln/vln_trainer.py
@@ -3,6 +3,7 @@ from typing import Dict, List
 
 import numpy as np
 import torch
+from torch.optim.lr_scheduler import LambdaLR
 
 from habitat import Config, logger
 from habitat_baselines.common.base_trainer import BaseTrainer
@@ -10,6 +11,7 @@ from habitat_baselines.common.env_utils import construct_envs
 from habitat_baselines.common.baseline_registry import baseline_registry
 from habitat_baselines.common.rollout_storage import RolloutStorage
 from habitat_baselines.common.environments import get_env_class
+from habitat_baselines.common.tensorboard_utils import TensorboardWriter
 from habitat_baselines.common.utils import (
     batch_obs,
     generate_video,
@@ -26,11 +28,71 @@ class VLNTrainer(BaseTrainer):
         self.config = config
         #if config is not None:
         #    logger.info(f"config: {config}")
+def _collect_rollout_step(
+    self, rollouts, current_episode_reward, episode_rewards, episode_counts
+):
+    pth_time = 0.0
+    env_time = 0.0
+
+    t_sample_action = time.time()
+
+    # sample actions
+    with torch.no_grad():
+        step_observation = {
+            k: v[rollouts.step] for k, v in rollouts.observations.items()
+        }
+
+        (
+            values,
+            actions,
+            actions_log_probs,
+            recurrent_hidden_states,
+        ) = self.actor_critic.act(
+            step_observation,
+            rollouts.recurrent_hidden_states[rollouts.step],
+            rollouts.prev_actions[rollouts.step],
+            rollouts.masks[rollouts.step],
+        )
+
+    pth_time += time.time() - t_sample_action
+
+    t_step_env = time.time()
+
+    outputs = self.envs.step([a[0].item() for a in actions])
+    observations, rewards, dones, infos = [list(x) for x in zip(*outputs)]
+
+    env_time += time.time() - t_step_env
+
+    t_update_stats = time.time()
+    batch = batch_obs(observations)
+    rewards = torch.tensor(rewards, dtype=torch.float)
+    rewards = rewards.unsqueeze(1)
+
+    masks = torch.tensor(
+        [[0.0] if done else [1.0] for done in dones], dtype=torch.float
+    )
+
+    current_episode_reward += rewards
+    episode_rewards += (1 - masks) * current_episode_reward
+    episode_counts += 1 - masks
+    current_episode_reward *= masks
+
+    rollouts.insert(
+        batch,
+        recurrent_hidden_states,
+        actions,
+        actions_log_probs,
+        values,
+        rewards,
+        masks,
+    )
+
+    pth_time += time.time() - t_update_stats
+
+    return pth_time, env_time, self.envs.num_envs
 
     def train(self):
-        print("VLNTrainer train method")
         # Get environments for training
-        print(get_env_class(self.config.ENV_NAME))
         self.envs = construct_envs(
             self.config, get_env_class(self.config.ENV_NAME)
         )
@@ -78,6 +140,118 @@ class VLNTrainer(BaseTrainer):
         batch = None
         observations = None
 
+        episode_rewards = torch.zeros(self.envs.num_envs, 1)
+        episode_counts = torch.zeros(self.envs.num_envs, 1)
+        #current_episode_reward = torch.zeros(self.envs.num_envs, 1)
+        #window_episode_reward = deque(maxlen=ppo_cfg.reward_window_size)
+        #window_episode_counts = deque(maxlen=ppo_cfg.reward_window_size)
+
+        t_start = time.time()
+        env_time = 0
+        pth_time = 0
+        count_steps = 0
+        count_checkpoints = 0
+
+        lr_scheduler = LambdaLR(
+            optimizer=self.agent.optimizer,
+            lr_lambda=lambda x: linear_decay(x, self.config.NUM_UPDATES),
+        )
+
+        with TensorboardWriter(
+            self.config.TENSORBOARD_DIR, flush_secs=self.flush_secs
+        ) as writer:
+            for update in range(self.config.NUM_UPDATES):
+                if cfg.use_linear_lr_decay:
+                    lr_scheduler.step()
+
+                if cfg.use_linear_clip_decay:
+                    self.agent.clip_param = cfg.clip_param * linear_decay(
+                        update, self.config.NUM_UPDATES
+                    )
+                # Main cycle until episode end or num_steps is reached
+                for step in range(cfg.num_steps):
+                    delta_pth_time, delta_env_time, delta_steps = self._collect_rollout_step(
+                        rollouts,
+                        current_episode_reward,
+                        episode_rewards,
+                        episode_counts,
+                    )
+
+                    pth_time += delta_pth_time
+                    env_time += delta_env_time
+                    count_steps += delta_steps
+
+                delta_pth_time, value_loss, action_loss, dist_entropy = self._update_agent(
+                    ppo_cfg, rollouts
+                )
+                pth_time += delta_pth_time
+
+                window_episode_reward.append(episode_rewards.clone())
+                window_episode_counts.append(episode_counts.clone())
+
+                losses = [value_loss, action_loss]
+                stats = zip(
+                    ["count", "reward"],
+                    [window_episode_counts, window_episode_reward],
+                )
+                deltas = {
+                    k: (
+                        (v[-1] - v[0]).sum().item()
+                        if len(v) > 1
+                        else v[0].sum().item()
+                    )
+                    for k, v in stats
+                }
+                deltas["count"] = max(deltas["count"], 1.0)
+
+                writer.add_scalar(
+                    "reward", deltas["reward"] / deltas["count"], count_steps
+                )
+
+                writer.add_scalars(
+                    "losses",
+                    {k: l for l, k in zip(losses, ["value", "policy"])},
+                    count_steps,
+                )
+
+                # log stats
+                if update > 0 and update % self.config.LOG_INTERVAL == 0:
+                    logger.info(
+                        "update: {}\tfps: {:.3f}\t".format(
+                            update, count_steps / (time.time() - t_start)
+                        )
+                    )
+
+                    logger.info(
+                        "update: {}\tenv-time: {:.3f}s\tpth-time: {:.3f}s\t"
+                        "frames: {}".format(
+                            update, env_time, pth_time, count_steps
+                        )
+                    )
+
+                    window_rewards = (
+                        window_episode_reward[-1] - window_episode_reward[0]
+                    ).sum()
+                    window_counts = (
+                        window_episode_counts[-1] - window_episode_counts[0]
+                    ).sum()
+
+                    if window_counts > 0:
+                        logger.info(
+                            "Average window size {} reward: {:3f}".format(
+                                len(window_episode_reward),
+                                (window_rewards / window_counts).item(),
+                            )
+                        )
+                    else:
+                        logger.info("No episodes finish in current window")
+
+                # checkpoint model
+                if update % self.config.CHECKPOINT_INTERVAL == 0:
+                    self.save_checkpoint(f"ckpt.{count_checkpoints}.pth")
+                    count_checkpoints += 1
+
+            self.envs.close()
 
     def save_checkpoint(self):
         ''' Snapshot models '''