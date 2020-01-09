0c64f284eb7bc5785ec33f5964d49f943d06d7e8
erick mendieta <erick84mm@gmail.com>
2020-01-09 19:28:39 +0900

    changing the adjacentviewpoints observation space

--- habitat_baselines/common/env_utils.py
+++ habitat_baselines/common/env_utils.py
@@ -88,7 +88,7 @@ def construct_envs(
         config.TASK_CONFIG = task_config
         config.freeze()
         configs.append(config.clone())
-
+    print("Spawning %s of envs" % len(configs))
     envs = habitat.VectorEnv(
         make_env_fn=make_env_fn,
         env_fn_args=tuple(
