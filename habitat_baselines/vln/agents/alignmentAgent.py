ac1e727e152b44719c0c4b660ed85e8207f0ae2c
erick mendieta <erick84mm@gmail.com>
2020-01-16 11:51:42 +0900

    Adding the feature extraction mechanism

--- habitat_baselines/vln/agents/alignmentAgent.py
+++ habitat_baselines/vln/agents/alignmentAgent.py
@@ -114,8 +114,7 @@ class alignmentAgent(habitat.Agent):
         # Get RPN proposals
         # proposal_generator inputs are the images, features, gt_instances
         # since is detect we don't need the gt instances
-        proposals, _ = self.detector.model.proposal_generator
-                        (
+        proposals, _ = self.detector.model.proposal_generator(
                             img_input,
                             features,
                             None
