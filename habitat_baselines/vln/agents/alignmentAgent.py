7d98e4197b7d2a16ec4fb2344df2275ffdf15d18
erick mendieta <erick84mm@gmail.com>
2020-01-22 14:41:44 +0900

    updating the loss

--- habitat_baselines/vln/agents/alignmentAgent.py
+++ habitat_baselines/vln/agents/alignmentAgent.py
@@ -166,7 +166,7 @@ class alignmentAgent(habitat.Agent):
 
 
     def train_step(self, steps):
-        self.loss = self.loss / self.max_steps
+        #self.loss = self.loss / self.max_steps
         self.loss.backward()
         self.loss = None
 
