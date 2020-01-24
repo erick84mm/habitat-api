020f18b112d95ad95e083bd681b833d240d39892
erick mendieta <erick84mm@gmail.com>
2020-01-25 08:02:21 +0900

    adding action history to bert

--- examples/Alignment_example.py
+++ examples/Alignment_example.py
@@ -98,15 +98,14 @@ class VLNBenchmark(habitat.Benchmark):
 
                 # add padding at the end
                 observations["actions"] = action_tokens
-                print(action_tokens)
                 observations["tokens"] = \
                     self._env._current_episode.instruction.tokens
                 observations["mask"] = \
                     self._env._current_episode.instruction.mask + \
                     action_mask
                 observations["segment"] = \
-                    [0] * len(self._env._current_episode.instruction.tokens)#+ \
-                    #action_segment_ids
+                    [0] * len(self._env._current_episode.instruction.tokens)+ \
+                    action_segment_ids
 
                 rollout_observations.append(observations)
                 action_sequence.append(action_idx)
