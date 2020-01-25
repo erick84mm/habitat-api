8b5e5de8a6770e32064f5d42258412aec0c1f9c7
erick mendieta <erick84mm@gmail.com>
2020-01-26 01:56:49 +0900

    adding eval, plus save, checkpoint

--- habitat_baselines/vln/agents/alignmentAgent.py
+++ habitat_baselines/vln/agents/alignmentAgent.py
@@ -847,7 +847,7 @@ class alignmentAgent(habitat.Agent):
         self.save["box_probs"].append(vision_logit.tolist())
         self.save["text"].append(linguistic_tokens.tolist())
         self.save["actions"].append(action)
-        print(type(vision_logit.tolist()), type(linguistic_tokens.tolist()), type(spatials[0].tolist()), type(action), type(ob["path_id"]))
+        print(vision_logit.tolist(), linguistic_tokens.tolist(), spatials[0].tolist())
 
         next_action = {"action": action, "action_args": action_args}
         if action == "TELEPORT":
