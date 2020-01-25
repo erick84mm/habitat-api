8bd998e883a939efaf00b55e5fd11f5a5a57b569
erick mendieta <erick84mm@gmail.com>
2020-01-26 06:04:28 +0900

    adding eval, plus save, checkpoint

--- habitat_baselines/vln/agents/alignmentAgent.py
+++ habitat_baselines/vln/agents/alignmentAgent.py
@@ -928,7 +928,7 @@ class alignmentAgent(habitat.Agent):
 
     def save(self, path):
         ''' Snapshot models '''
-        torch.save(self.model.state_dict(), path)
+        torch.save(self.model.state_dict, path)
 
     def load(self, path):
         ''' Loads parameters (but not training state) '''
