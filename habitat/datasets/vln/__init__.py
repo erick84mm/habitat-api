e4b81192de7fb384b9b30516b2ad155454bb0ef5
erick mendieta <erick84mm@gmail.com>
2020-02-14 13:30:10 +0900

    adding the previous task to the init

--- habitat/datasets/vln/__init__.py
+++ habitat/datasets/vln/__init__.py
@@ -7,7 +7,24 @@
 from habitat.core.dataset import Dataset
 from habitat.core.registry import registry
 
+def _try_register_r2r_vln_dataset():
+    try:
+        from habitat.datasets.vln.r2r_vln_dataset import VLNDatasetV1
 
+        has_r2r_vln = True
+    except ImportError as e:
+        has_r2r_vln = False
+        r2r_vln_import_error = e
+
+    if has_r2r_vln:
+        from habitat.datasets.vln.r2r_vln_dataset import VLNDatasetV1
+    else:
+
+        @registry.register_dataset(name="R2RVLN-v1")
+        class R2RDatasetImportError(Dataset):
+            def __init__(self, *args, **kwargs):
+                raise r2r_vln_import_error
+                
 def _try_register_r2r_dataset():
     try:
         from habitat.datasets.vln.r2r_dataset import (
